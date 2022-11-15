3fbef286361ba13b9c1453aaa078c2808cfb7667
MattScarthSaunders <m.scarsaund@gmail.com>
2022-11-15 13:52:19 +0000

    patch error handling progress

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -202,5 +202,14 @@ describe("PATCH", () => {
           expect(res.body.msg).toBe("Received invalid content");
         });
     });
+    test("PATCH 400 - body prop typo", () => {
+      return request(app)
+        .patch("/api/reviews/1")
+        .send({ inc_vetes: 1 })
+        .expect(400)
+        .then((res) => {
+          expect(res.body.msg).toBe("Received invalid content");
+        });
+    });
   });
 });
--- controllers/errors.controllers.js
+++ controllers/errors.controllers.js
@@ -1,3 +1,5 @@
+const { checkExists } = require("../utils/utils");
+
 exports.catchAll = (err, req, res, next) => {
   console.log(err, "unhandled error");
   res.status(500).send({ msg: "internal server error" });
